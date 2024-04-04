5edaeb4865b8232b36c554da12d2b8484b284cbd
thangdv <duongthang123hd@gmail.com>
2024-04-04 21:16:59 +0700

    create database

--- database/migrations/2024_04_04_125732_create_products_table.php
+++ database/migrations/2024_04_04_125732_create_products_table.php
@@ -11,16 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('phone')->unique();
-            $table->text('address')->nullable();
-            $table->string('gender')->default('male');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->text('description');
+            $table->double('sale')->default(0);
+            $table->double('price');
+            $table->integer('active');
             $table->timestamps();
         });
     }
@@ -30,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 };
