f218302ff047b97334fc246d6101ce45edf1d90f
thangdv <duongthang123hd@gmail.com>
2024-04-16 17:58:16 +0700

    layout homepage and show product in homepage

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,17 @@
+<nav class="header__menu mobile-menu">
+    <ul>
+        <li class="active"><a href="{{ route('home') }}">Trang chủ</a></li>
+        <li><a href="./shop.html">Cửa hàng</a></li>
+        <li><a href="#">Pages</a>
+            <ul class="dropdown">
+                <li><a href="./about.html">About Us</a></li>
+                <li><a href="./shop-details.html">Shop Details</a></li>
+                <li><a href="./shopping-cart.html">Shopping Cart</a></li>
+                <li><a href="./checkout.html">Check Out</a></li>
+                <li><a href="./blog-details.html">Blog Details</a></li>
+            </ul>
+        </li>
+        <li><a href="./blog.html">Tin tức</a></li>
+        <li><a href="./contact.html">Liên hệ</a></li>
+    </ul>
+</nav>
---
+++ resources/views/layouts/slider.blade.php
@@ -0,0 +1,85 @@
+<section class="hero" style="padding-bottom:100px ">
+    <div class="hero__slider owl-carousel owl-loaded owl-drag">
+        <div class="owl-stage-outer">
+            <div class="owl-stage"
+                 style="transform: translate3d(-3348px, 0px, 0px); transition: all 0s ease 0s; width: 6696px;">
+                <div class="owl-item cloned" style="width: 1116px;">
+                    <div class="hero__items set-bg" data-setbg="{{asset('client/img/hero/hero-1.jpg')}}"
+                         style="background-image: url(&quot;img/hero/hero-1.jpg&quot;);">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-xl-5 col-lg-7 col-md-8">
+                                    <div class="hero__text">
+                                        <h6>Mùa hè rực lửa</h6>
+                                        <h2>Rực lửa mùa Euro - 2024</h2>
+                                        <p>Khám phá - Thỏa sức cháy với cùng trang phục các đội tuyển quốc gia.</p>
+                                        <a href="#" class="primary-btn">Mua ngay<span class="arrow_right"></span></a>
+                                        <div class="hero__social">
+                                            <a href="#"><i class="fa fa-facebook"></i></a>
+                                            <a href="#"><i class="fa fa-twitter"></i></a>
+                                            <a href="#"><i class="fa fa-pinterest"></i></a>
+                                            <a href="#"><i class="fa fa-instagram"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="owl-item animated owl-animated-in fadeIn cloned" style="width: 1116px;">
+                    <div class="hero__items set-bg" data-setbg="{{asset('client/img/hero/hero-2.jpg')}}"
+                         style="background-image: url(&quot;img/hero/hero-2.jpg&quot;);">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-xl-5 col-lg-7 col-md-8">
+                                    <div class="hero__text">
+                                        <h6>Ngoại Hạng Anh - 2024</h6>
+                                        <h2>Mùa giải - 2023/2024</h2>
+                                        <p>Khám phá - Thỏa sức cháy với cùng trang phục các câu lạc bộ.</p>
+                                        <a href="#" class="primary-btn">Mua ngay<span class="arrow_right"></span></a>
+                                        <div class="hero__social">
+                                            <a href="#"><i class="fa fa-facebook"></i></a>
+                                            <a href="#"><i class="fa fa-twitter"></i></a>
+                                            <a href="#"><i class="fa fa-pinterest"></i></a>
+                                            <a href="#"><i class="fa fa-instagram"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="owl-item" style="width: 1116px;">
+                    <div class="hero__items set-bg" data-setbg="{{asset('client/img/hero/hero-1.jpg')}}"
+                         style="background-image: url(&quot;img/hero/hero-1.jpg&quot;);">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-xl-5 col-lg-7 col-md-8">
+                                    <div class="hero__text">
+                                        <h6>Bộ sưu tập mùa hè</h6>
+                                        <h2>Rực lửa mùa Euro - 2024</h2>
+                                        <p>Khám phá - Thỏa sức cháy với cùng trang phục các đội tuyển quốc gia.</p>
+                                        <a href="#" class="primary-btn">Mua ngay<span class="arrow_right"></span></a>
+                                        <div class="hero__social">
+                                            <a href="#"><i class="fa fa-facebook"></i></a>
+                                            <a href="#"><i class="fa fa-twitter"></i></a>
+                                            <a href="#"><i class="fa fa-pinterest"></i></a>
+                                            <a href="#"><i class="fa fa-instagram"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="owl-nav">
+            <button type="button" role="presentation" class="owl-prev"><span class="arrow_left"><span></span></span>
+            </button>
+            <button type="button" role="presentation" class="owl-next"><span class="arrow_right"><span></span></span>
+            </button>
+        </div>
+        <div class="owl-dots disabled"></div>
+    </div>
+</section>
