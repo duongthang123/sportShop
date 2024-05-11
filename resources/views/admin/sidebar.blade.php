28afa8179d6ff94f0c55d921a8d2bfe800d0698c
thangdv <duongthang123hd@gmail.com>
2024-05-11 17:16:27 +0700

    update logo user

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -2,7 +2,7 @@
     <!-- Sidebar user panel (optional) -->
     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-            <img src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png" class="img-circle elevation-2" alt="User Image">
+            <img src="{{ \Illuminate\Support\Facades\Auth::user()->image_path }}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
             <a href="#" class="d-block">{{\Illuminate\Support\Facades\Auth::user()->name}}</a>
